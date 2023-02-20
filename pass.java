dbd9adb14658b21be07db60ff26f8656317631b3
yosefismyname <tugaskelompok1asj@gmail.com>
2023-02-20 19:49:06 +0700

    Menambahkan file pass.java

---
+++ pass.java
@@ -0,0 +1,43 @@
+public class enkripsiGenerator {
+
+    public static String enkripsiGenerator(String data) {
+
+        String encryptedpassword = null;
+        try {
+
+            MessageDigest m = MessageDigest.getInstance("MD5");
+
+            m.update(data.getBytes());
+
+            byte[] bytes = m.digest();
+
+            StringBuilder s = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+
+            encryptedpassword = s.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        String hasil = encryptedpassword;
+        return hasil;
+    }
+
+
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
